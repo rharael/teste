f794e17884129e24685f0183414c6c4c6678e4ca
Wendel Rharael <60363332+rharael@users.noreply.github.com>
2024-11-26 12:30:48 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {Text, View } from 'react-native';
+import Screen from './src/components/Screen';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+	<Screen>
+    <View>
+      <Text>Em edição...</Text>
       <StatusBar style="auto" />
     </View>
+	</Screen>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/components/Screen.js
@@ -0,0 +1,14 @@
+import { Platform, SafeAreaView, StatusBar } from 'react-native';
+import styled from 'styled-components';
+
+const SafeScreen = styled.SafeAreaView`
+padding-top: ${Platform.OS === 'android' ? StatusBar.currentHeight : 0}
+`;
+
+export default function Screen({children}) {
+ return (
+   <SafeScreen>
+	{children}
+   </SafeScreen>
+  );
+}
