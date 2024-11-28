f8537e1b154e2241eaa34dbc19b43a07ffce062f
JoaoGabrielBastos <joaogabriel0879@gmail.com>
2024-11-28 10:31:15 -0300

    empty favorites screen finished

--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -2,18 +2,20 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex:1;
-    justify-contente:center;
+    justify-content:center;
     align-items: center;
     background-color:#FFF;
 `;
 
-export const Title = style.Text`
-font-size: 28px;
+
+export const Title = styled.Text`
+font-size: 16px;
 color: #2A2A2A;
-font-wheight:600:
+justify-content: center;
+font-weight: bold;
 `;
 
-export const Image = style.Image`
-    width: 226px;
-    height: 226px;
+export const Image = styled.Image`
+width: 226px;
+height: 226px;
 `;
